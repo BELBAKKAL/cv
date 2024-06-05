8121d174c6884bf15cdfed8e6922bbdb2c8b9cbb
Youssef belbakkal <youssefbelbakkal485@gmail.com>
2024-06-06 00:21:45 +0100

    2 commit

--- reactProject-main/src/components/Formcomponents/FooterF.tsx
+++ reactProject-main/src/components/Formcomponents/FooterF.tsx
@@ -1,9 +1,9 @@
 export const FooterF = () => {
     return (
         <footer className="mt-4 text-center border-t p-3 font-bold">
-        <p>Edité par : EL MEJBAR Otmane</p>
-        <p>Dernière mise à jour : 09/11/2023</p>
-        <p>Copyright reserved</p>
+ <p>Edité par : BELBAKKAL YOUSSEF</p>
+            <p>Dernière mise à jour : 05/06/2024</p>
+            <p>Copyright reserved</p>
       </footer>
     );
 }
--- src/components/CVcomponents/Presentation.tsx
+++ src/components/CVcomponents/Presentation.tsx
@@ -3,11 +3,11 @@ export const Presentation = () => {
         <div>
             <section id="nom_prenom" className="text-center mt-4 font-bold">
                 <p>
-                    <span className="font-semibold">Nom:</span> ELMEJBAR <br></br>
-                    <span className="font-semibold">Prénom:</span> Otmane
+                    <span className="font-semibold">Nom:</span> BELBAKKAL <br></br>
+                    <span className="font-semibold">Prénom:</span> Youssef
                 </p>
                 <p>
-                    <span className="font-semibold">Age:</span> 20 ans<br></br>
+                    <span className="font-semibold">Age:</span> 22 ans<br></br>
                     <span className="font-semibold">Adresse:</span> Lot. Arimas, rue 9, n 82, Tanger
                 </p>
             </section>
